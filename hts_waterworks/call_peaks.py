50233961146a3185117c840ad3081f6581b19332
Jake Biesinger <jake.biesinger@gmail.com>
2012-03-01 14:21:06 -0800

    Support for QuEST, use MACS --diag

--- hts_waterworks/call_peaks.py
+++ hts_waterworks/call_peaks.py
@@ -6,6 +6,7 @@
 import re
 import itertools
 import tempfile
+import shutil
 
 from ruffus import (transform, follows, collate, files, split, merge,
                     add_inputs, regex, suffix, mkdir, jobs_limit, output_from)
@@ -13,7 +14,7 @@ from ruffus.task import active_if
 
 from hts_waterworks.utils.ruffus_utils import (sys_call, main_logger as log,
                                            main_mutex as log_mtx)
-from hts_waterworks.bootstrap import cfg, get_chrom_sizes
+from hts_waterworks.bootstrap import cfg, get_chrom_sizes, genome_path
 import hts_waterworks.mapping as mapping
 import hts_waterworks.clip_seq as clip_seq
 from hts_waterworks.utils.common import (bedCommentFilter, readBedLines,
@@ -83,7 +84,7 @@ def run_macs14(in_files, out_peaks, max_fdr):
     in_treat, in_control = in_files[0]
     matches = re.search(r'(.*\.treat)(.*)\.mapped_reads', in_treat).groups()
     name = matches[0] + matches[1] + '.macs14.peaks'
-    cmd = 'macs14 -t %s -c %s --name=%s %s' % (in_treat, in_control, name,
+    cmd = 'macs14 -t %s -c %s --name=%s %s --diag' % (in_treat, in_control, name,
                                              cfg.get('peaks', 'macs14_params'))
     sys_call(cmd)
     peaks_to_keep = set()
@@ -228,23 +229,25 @@ def run_arem(in_files, out_peaks, max_fdr):
                                         'AREM_peak_%s' % (index + 1), score])
                                                 + '\t+\n')
 
-
 @active_if(cfg.getboolean('peaks', 'run_glitr'))
 @transform(mapping.all_mappers_output,
-    suffix('.mapped_reads'), '.mapped_reads_starts')
+    suffix('.mapped_reads'), '.mapped_reads_glitr')
 def bed_to_glitr(in_bed, out_starts):
-    """Convert reads to (chrom, start, strand) for GLITR."""
+    """Convert reads to (chrom, start, strand) for GLITR"""
     with open(in_bed) as infile:
         with open(out_starts, 'w') as outfile:
             for chrom, start, stop, strand in readBedLines(infile):
                 outfile.write('\t'.join([chrom, str(start), strand]) + '\n')
 
+@active_if(cfg.getboolean('peaks', 'run_glitr'))
 @jobs_limit(cfg.getint('DEFAULT', 'max_throttled_jobs'), 'throttled')
-@collate(bed_to_glitr, regex(r'(.+)\.treat\.mapped_reads_starts'), 
-        add_inputs(r'\1.control.mapped_reads_starts'), r'\1.treat.glitr.ranges')
+@collate(bed_to_glitr,
+         regex(r'(.+)\.(treat|control)\.(.+)\.mapped_reads_glitr$'),
+         r'\1.treat.\3.glitr.ranges')
 def run_glitr(in_files, out_peaks):
     """Call peaks with GLITR"""
-    in_treat, in_control = in_files[0]
+    in_treat = filter(lambda f: '.treat.' in f, in_files)[0]
+    in_control = filter(lambda f: '.control.' in f, in_files)[0]
     glitr_dir = in_treat + '.GLITR_out'
     cmd = ('rm -r %s; mkdir %s; cd %s; GLITR.pl --CHIP=../%s ' + \
             '--CONTROL=../%s --GENOME=%s %s ') % (
@@ -252,7 +255,7 @@ def run_glitr(in_files, out_peaks):
                 cfg.get('DEFAULT', 'genome').upper(),
                 cfg.get('peaks', 'glitr_params'))
     sys_call(cmd)
-    sys_call('cp %s/allChIP.FDR_1.5PercentFDR %s' % (glitr_dir, out_peaks))
+    sys_call('cp %s/allChIP.FDR_*PercentFDR %s' % (glitr_dir, out_peaks))
 
 @transform(run_glitr, suffix('.glitr.ranges'), '.glitr.peaks')
 def glitr_range_to_bed(in_range, out_bed):
@@ -279,6 +282,44 @@ def glitr_range_to_bed(in_range, out_bed):
                                     str(center_stop), 'GLITR_peak_%s'%(i+1),
                                     str(int(float(foldchange))),'+']) + '\n')
 
+@active_if(cfg.getboolean('peaks', 'run_QuEST'))
+@transform(mapping.all_mappers_output,
+    suffix('.mapped_reads'), '.mapped_reads_quest')
+def bed_to_quest(in_bed, out_regions):
+    """Convert bed file input to space-delimited positions"""
+    with open(in_bed) as infile:
+        with open(out_regions, 'w') as outfile:
+            for line in infile:
+                fields = line.strip().split('\t')
+                outfile.write(' '.join(fields[:2] + [fields[5]]) + '\n')
+
+@collate(bed_to_quest,
+         regex(r'(.*)\.(treat|control)\.(.*)\.mapped_reads_quest$'),
+         r'\1.treat.\3.quest.peaks', '%s.chrom.sizes' % genome_path())
+def run_quest(in_reads, out_peaks, chrom_sizes):
+    """Run QuEST on the given treatment and control data"""
+    in_treat = filter(lambda f: '.treat.' in f, in_reads)[0]
+    in_control = filter(lambda f: '.control.' in f, in_reads)[0]
+    sys_call('echo "y\n1\n2\ny\n" | generate_QuEST_parameters.pl -QuEST_align_ChIP %s '
+             '-QuEST_align_RX_noIP %s -gt %s -ap %s_output -silent' %
+             (in_treat, in_control, chrom_sizes, in_treat))
+    shutil.copy('%s_output/calls/peak_caller.ChIP.out.accepted' % in_treat, out_peaks)
+
+@follows(run_quest)
+@split(bed_to_quest, regex(r'(.*)\.treat\.(.*)\.mapped_reads_quest$'),
+       r'\1.treat.\2.quest.*.wig',
+       r'\1.treat.\2.quest.%s.wig',
+       r'\1.treat.\2.mapped_reads_quest_output',
+       '%s.chrom.sizes' % genome_path())
+def quest_to_wig(in_reads, out_glob, out_template, in_dir, chrom_sizes):
+    in_template = in_dir + '/tracks/wig_profiles/%s.profile.wig.gz'
+    for f in ['background_unnormalized', 'background_normalized', 'ChIP_normalized', 'ChIP_unnormalized']:
+        in_file = in_template % f
+        out_file = out_template % f
+        #shutil.copy(in_file, out_file)
+        sys_call('gunzip -c -d %s > %s' % (in_file, out_file))
+
+
 @active_if(cfg.getboolean('motifs', 'discover_read_pileup_motifs'))
 @transform(clip_seq.pileup_starts, suffix('.pileup_reads'), '.treat.pileup.peaks')
 def pileup_as_peaks(in_pileup, out_peaks):
@@ -289,6 +330,19 @@ def pileup_as_peaks(in_pileup, out_peaks):
             strand = '-' if 'minus' in in_pileup else '+'
             outfile.write('\t'.join([chrom, start, stop, '.', count, strand]) + '\n')
 
+#def reproducible_as_peaks(in_pickle, out_peaks):
+#    # write out reads that pass min read criteria in all datasets
+#    min_count = 10
+#    out_passing = {}
+#    for exp in in_pileups:
+#        out_passing[exp] = open(out_pattern % exp, 'w')
+#    for pos in count_by_position:
+#        if sum(count_by_position[pos] >= mincount) >= 3:
+#            for expindex, exp in enumerate(in_pileups):
+#                if count_by_position[pos][expindex] > 0:
+#                    out_passing[exp].write('\t'.join(map(str, pos + (int(pos[1]) + 1, '.', count_by_position[pos][expindex], '+'))) + '\n')
+
 all_peak_caller_functions = [run_macs, run_macs14, run_macs14_no_control,
-                             run_arem, glitr_range_to_bed, pileup_as_peaks]
+                             run_arem, glitr_range_to_bed, pileup_as_peaks,
+                             clip_seq.reproducible_positions]
 
